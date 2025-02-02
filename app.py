0a2e7b43c1ced28769fbeee1ba8cb88aa9b98701
mukesh <mukesh@gmail.com>
2025-02-02 15:53:26 +0545

    Changes

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ def webout():
  return '<h1>DevOps is fun.</h1>'
  return '<h1>DevOps is tooooooooooooooooo fun.</h1>'
  return '<h1>Sanfebagar Mun.</h1>'
+ return '<h1>Sanfebagar Mun.</h1>'
+ return '<h1>Chaurpati r.m Mun.</h1>'
+ return '<h1>Sanfebagar Mun.</h1>'
 app.run(host='0.0.0.0',port=7000)
